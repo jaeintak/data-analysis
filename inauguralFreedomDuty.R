0b9fe483ca71236e0b6e81a5351d7887b94bf5da
Jae In Tak <takjaein@sogang.ac.kr>
2019-09-05 19:13:30 +0900

    Delete inauguralFreedomDuty.R

--- inauguralFreedomDuty.R
+++
@@ -1,9 +0,0 @@
-Keywords <- data.frame(Freedom=vector(), Duty=vector())
-n = 0
-for (j in list.files()) {
-file <- scan(file=j, what="char", quote=NULL)
-n = n + 1
-file <- gsub("^[[:punct:]]+|[[:punct:]]$", "", tolower(file))
-Keywords[n, ] <- c(length(file[grep("^freedom", file)])/length(file), length(file[file=="duty"|file=="duties"])/length(file))
-rownames(Keywords)[n] <- j
-}
